9496a0815c8bfca6a5638de6fe37079d8f335af7
Jonathan Darakdjian <jonydarakdjian@gmail.com>
2024-04-03 17:33:24 -0300

    Add LogoCircle component and social media icons

---
+++ src/components/logoCircle/types.ts
@@ -0,0 +1,6 @@
+export interface LogoCircleProps {
+  logoComponent: React.ReactNode;
+  borderColor?: string;
+  backgroundColor?: string;
+  buttonProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
+}
