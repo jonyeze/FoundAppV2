2e2cccfd9b722839182addb82280621be6a99a8b
Jonathan Darakdjian <jonydarakdjian@gmail.com>
2024-04-06 18:25:12 -0300

    Add Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
 import Landing from "./pages";
 import SignIn from "./pages/login/signIn";
 
 const App: React.FC = () => {
   return <>
-    <Home />
     <SignIn />
   </>;
 };
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -6,7 +6,11 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
+<<<<<<< HEAD
     <div className="container m-auto flex w-full h-screen p-5  flex-col items-center">
+=======
+    <div className="container flex w-full h-screen p-10 flex-col items-center">
+>>>>>>> f76271539d1db25cd41aea66a473394da32787e0
       {children}
     </div>
   );
